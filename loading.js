d5abf26cabc9e5926d637ef8cbda012310ad90e6
orde <130056473+orde2201@users.noreply.github.com>
2025-02-14 00:30:27 +0700

    Add files via upload

---
+++ loading.js
@@ -0,0 +1,7 @@
+var loading=false;
+
+window.onload = function(){
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("content").style.display = "block";
+}
+
