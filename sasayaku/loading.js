347df6b0d9e96c16ec170cb53e30e0fcabece693
orde <130056473+orde2201@users.noreply.github.com>
2025-02-14 00:28:47 +0700

    Delete sasayaku directory

--- sasayaku/loading.js
+++
@@ -1,7 +0,0 @@
-var loading=false;
-
-window.onload = function(){
-    document.getElementById("loader").style.display = "none";
-    document.getElementById("content").style.display = "block";
-}
-
