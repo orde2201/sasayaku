1cd591a2d34147f624c4af3c8237177b7aa69767
orde <130056473+orde2201@users.noreply.github.com>
2025-03-10 21:28:18 +0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,49 @@
+$(".char1").click(function(){
+    $(".yori").fadeIn();
+    $(".yori").css({
+        "margin-bottom":"-12vw"
+       })
+})
+$(".char1").mouseleave(function(){
+    $(".yori").fadeOut();
+})
+
+$(".char2").click(function(){
+    $(".kino").fadeIn();
+    $(".kino").css({
+        "margin-bottom":"-11vw",
+        
+       })
+})
+$(".char2").mouseleave(function(){
+    $(".kino").fadeOut();
+})
+
+$(".char3").click(function(){
+    $(".aki").fadeIn();
+    $(".aki").css({
+        "margin-bottom":"-11vw"
+       })
+})
+$(".char3").mouseleave(function(){
+    $(".aki").fadeOut();
+})
+
+$(".char4").click(function(){
+    $(".mari").fadeIn();
+    $(".mari").css({
+        "margin-bottom":"-8.25vw"
+       })
+})
+$(".char4").mouseleave(function(){
+    $(".mari").fadeOut();
+})
+$(".char5").click(function(){
+    $(".tachibana").fadeIn();
+    $(".tachibana").css({
+        "margin-bottom":"-7vw"
+       })
+})
+$(".char5").mouseleave(function(){
+    $(".tachibana").fadeOut();
+})
